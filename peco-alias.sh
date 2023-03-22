2870f93e03e0053ab7f4455ee95e99a78aa30adb
sjbworks <sacred.jizo.bosatz@gmail.com>
2023-03-23 00:10:03 +0900

    Add command, process kill, change directory for ghq, git config

--- peco-alias.sh
+++ peco-alias.sh
@@ -1,5 +1,5 @@
 # peco
-## ctrl + r: 過去に実行したコマンドを選択できるようにする
+# ctrl + r: Select a command done past
 function peco-select-history() {
   BUFFER=$(\history -n -r 1 | peco --query "$LBUFFER")
   CURSOR=$#BUFFER
@@ -8,15 +8,30 @@ function peco-select-history() {
 zle -N peco-select-history
 bindkey '^r' peco-select-history
 
-## checkoutする
-alias gco='git checkout $(git branch | sed -r "s/^[ \*]+//" | peco)'
-
-## switchする
-alias gs='git switch $(git branch | sed -r "s/^[ \*]+//" | peco)'
-
-## ctrl + j: change directory for ghq
-function peco-cd {
-  cd "$( ghq list --full-path | peco)"
+# Select a working process and kill
+function peco-pkill() {
+  for pid in `ps aux | peco | awk '{ print $2 }'`
+  do
+    kill $pid
+    echo "Killed ${pid}"
+  done
 }
+alias pk="peco-pkill"
 
-alias sd='peco-cd'
+# Change directory for ghq
+function pcd {
+  code "$( ghq list --full-path | peco)"
+}
+alias sd='pcd'
+
+# git
+alias -g g='git'
+alias -g B='`git branch -a | peco --prompt "GIT BRANCH>" | head -n 1 | sed -e "s/^\*\s*//g"`'
+alias -g R='`git remote | peco --prompt "GIT REMOTE>" | head -n 1`'
+alias -g gsw='git switch $(git branch | sed -r "s/^[ \*]+//" | peco)'
+git config --global alias.a 'add'
+git config --global alias.b 'branch'
+git config --global alias.c 'commit -m'
+git config --global alias.co 'checkout'
+git config --global alias.p 'pull'
+git config --global alias.pu 'push'
